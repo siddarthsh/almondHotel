a0b23bd73119ba227e7b228ba077fa1336dde278
Siddarth Sharma <siddarthsharma1077@gmail.com>
2020-08-02 19:32:32 +0530

    Git Setup

---
+++ precache-manifest.518135a89bda2a9ed1f77f439567dda5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821ad8294dbfb28fb956487fdd40f03d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "572000d33e60693d3673",
+    "url": "/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "e9c9d9cddafade27089e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "572000d33e60693d3673",
+    "url": "/static/js/2.14dbb8f4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.14dbb8f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c9d9cddafade27089e",
+    "url": "/static/js/main.af35b6ce.chunk.js"
+  },
+  {
+    "revision": "14193636acd8321b2a95",
+    "url": "/static/js/runtime-main.6b94d589.js"
+  }
+]);
